72641bb3e0f5ebe5d9759b0d3f85b0df19976c61
Erik Waibel <ewaibel@itracs.com>
2013-11-19 11:50:17 -0700

    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+read -p "Have you installed homebrew?" -n 1
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+	ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+fi
+
+###############################################################################
+# Sublime Text                                                                #
+###############################################################################
+read -p "Do you want to copy Sublime Text settings?" -n 1
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	cp -r dev/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Preferences.sublime-settings 2> /dev/null
+fi
+
+###############################################################################
+# OSX Defaults                                                                #
+###############################################################################
+read -p "Do you want to update OSX defaults?" -n 1
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	cp home/.osx ~
+	source ~/.osx
+fi
+
+cp home/.bash_profile ~/.profile
+cp home/.functions ~
+cp home/.inputrc ~
+cp home/.gitconfig ~
+
+source ~/.profile

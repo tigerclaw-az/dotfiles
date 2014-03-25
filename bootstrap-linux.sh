d89c89ba9dd75c29cab0031a58a9240fae8ece52
Erik Waibel <ejwaibel@gmail.com>
2014-03-24 22:18:21 -0700

    Add check for installing packages and read package list from packages-linux.txt

--- bootstrap-linux.sh
+++ bootstrap-linux.sh
@@ -7,15 +7,30 @@ if [ $(whoami) != "root" ]; then
 fi
 
 ###############################################################################
-# Standard Linux packages                                                     #
+# Install Linux packages                                                     #
 ###############################################################################
-apt-get update
-apt-get install -y aptitude chromium-browser dos2unix fluxgui git git-core bash-completion nodejs php5 python ruby
+packagesFile="packages-linux.txt"
+apt-get update && apt-get upgrade
+read -p "Do you want to install packages [yN]? " -n 1
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	filecontent=( `cat $packagesFile `)
+
+	for p in "${filecontent[@]}"
+	do
+		apt-get install -y $p
+		if [ $? != 0 ]; then
+			echo -e "\t\033[31mFAIL\033[0m"
+		else
+			echo -e "\t\033[32mSUCCESS\033[0m"
+		fi
+	done
+fi
 
 ###############################################################################
 # Macbuntu Theme                                                              #
 ###############################################################################
-read -p "Do you want to install the Macbuntu theme? " -n 1
+read -p "Do you want to install the Macbuntu theme [yN]? " -n 1
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
 	wget https://downloads.sourceforge.net/project/macbuntu/macbuntu-10.10/v2.3/Macbuntu-10.10.tar.gz -O /tmp/Macbuntu-10.10.tar.gz

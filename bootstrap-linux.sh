aaeda22c426b13b8a3117e20ee33d056f50bfe3a
Erik Waibel <ejwaibel@gmail.com>
2014-04-10 21:57:40 -0700

    Add better output messages for bootstrap-linux.sh

--- bootstrap-linux.sh
+++ bootstrap-linux.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cwd=pwd
+cwd=$(pwd)
 
 if [ $(whoami) != "root" ]; then
 	echo "ERROR: This script must be run as root!"
@@ -10,11 +10,12 @@ fi
 # Install Linux packages                                                     #
 ###############################################################################
 packagesFile="packages-linux.txt"
-apt-get update && apt-get upgrade
-read -p "Do you want to install packages [yN]? " -n 1
+echo -e "Performing apt-get update & upgrade..."
+(apt-get update && apt-get upgrade) > /dev/null 2>&1
+filecontent=( `cat $packagesFile `)
+echo -e "\nThe following packages will be installed or upgraded:\n\t\033[33m${filecontent[@]}\033[0m\n"
+read -p "Do you want to install packages?[yN] " -n 1
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	filecontent=( `cat $packagesFile `)
-
 	for p in "${filecontent[@]}"
 	do
 		apt-get install -y $p
@@ -24,6 +25,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 			echo -e "\t\033[32mSUCCESS\033[0m"
 		fi
 	done
+	echo -e "--> Package installation completed!"
 fi
 
 ###############################################################################
@@ -32,19 +34,27 @@ fi
 read -p "Do you want to install the Macbuntu theme [yN]? " -n 1
 if [[ $REPLY =~ ^[Yy]$ ]]; then
 	wget https://downloads.sourceforge.net/project/macbuntu/macbuntu-10.10/v2.3/Macbuntu-10.10.tar.gz -O /tmp/Macbuntu-10.10.tar.gz
-	tar xzvf /tmp/Macbuntu-10.10.tar.gz -C /tmp
+	tar xzvf /tmp/Macbuntu-10.10.tar.gz -C /tmp > /dev/null 2>&1
 	cd /tmp/Macbuntu-10.10/
+	echo "Performing Macbuntu install..."
 	./install.sh
+	if [ $? != 0 ]; then
+		echo -e "\033[31mFAILED\033[0m"
+	else
+		echo -e "\033[32mSUCCEEDED\033[0m"
+	fi
 	cd $pwd
 fi
 
-ln -s home/.bash_profile.LINUX ~/.bashrc
-ln -s home/.functions ~/.functions
-ln -s home/.inputrc ~/.inputrc
-ln -s home/.gitconfig ~/.gitconfig
-ln -s home/.gitprompt ~/.gitprompt
+echo -e "Linking home files..."
+ln -s "$cwd/home/.bash_profile.LINUX" ~/.bashrc
+ln -s "$cwd/home/.functions" ~/.functions
+ln -s "$cwd/home/.inputrc" ~/.inputrc
+ln -s "$cwd/home/.gitconfig" ~/.gitconfig
+ln -s "$cwd/home/.gitprompt" ~/.gitprompt
 
 source ~/.bashrc
 
+echo -e "----"
 echo -e "Download Sublime Text: http://www.sublimetext.com/3"
 echo -e "Disable update manager: http://ubuntuforums.org/showthread.php?t=1966228\n"

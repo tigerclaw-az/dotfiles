3ad1b7e93c8818d3b1819319fc6638fee0454618
Erik Waibel <ejwaibel+github@gmail.com>
2018-02-01 18:25:47 -0700

    Update npm list based on latest packages

--- bin/npm.sh
+++ bin/npm.sh
@@ -1,2 +1,2 @@
-npm install -g yarn generator-gulp-webapp gulp jscs jshint eslint sass-lint node-inspector npm-check-updates yo xmllint
+npm install -g generator-webapp gulp jscs jshint eslint sass-lint node-inspector npm-check-updates yo xmllint
 

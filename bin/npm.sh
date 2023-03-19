504a665f82597dff39a910a5239aa0019e079ad2
Erik Waibel <ejwaibel@gmail.com>
2023-03-18 23:09:50 -0700

    Fix out npm packages are installed

--- bin/npm.sh
+++ bin/npm.sh
@@ -1,1 +1,13 @@
-npm install -g @angular/cli @prettier/plugin-php @vue/cli audiosprite gulp prettier eslint sass-lint node-inspector typescript yarn
+packages=(@angular/cli @prettier/plugin-php @vue/cli audiosprite gulp prettier
+	eslint sass-lint typescript yarn);
+
+for p in "${packages[@]}"
+	do
+		npm install -g $p
+		echo "\t${p}\n"
+		if [ $? != 0 ]; then
+			echo "\t\033[31mFAILED -> ${p}\033[0m"
+		else
+			echo "\t\033[32mSUCCESS\033[0m"
+		fi
+	done
